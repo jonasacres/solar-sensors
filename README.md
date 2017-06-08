2b7cbe330177405762326da2d2f353e424e86501
Jonas Acres <acresjonas@gmail.com>
2017-06-08 07:58:13 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# Introduction
+Measures the output of a solar panel and a small photoresistor, and relays it via http-keystore.
+
+# Motivation
+I wanted to know how well a solar panel would do where I live. Also, I wanted to round out my weather data with a measure of how much sunlight we were getting.
+
+# Environment
+
+* Sensors: Solar panel connected to GPIO pins of raspberry pi
+* Raspberry Pi: Pi 2 Model B running Raspbian 7.8.
+
+I followed the example of https://learn.adafruit.com/reading-a-analog-in-and-controlling-audio-volume-with-the-raspberry-pi/overview to wire the ADC in.
+
+ADC Pin | GPIO Pin
+------- | ---------------
+VDD     | +3.3V
+VREF    | +3.3V
+AGND    | GND
+CLK     | #18
+DOUT    | #23
+DIN     | #24
+CS      | #25
+DGND    | GND
+
+# How it works
+The panel has a 1K resistor on it as a load, and is connected to a MCP3008 ADC, which is in turn connected to the GPIO pins of a raspberry pi. Since the MCP3008 has 8 channels of input and I had a spare photoresistor laying around, I put that in too, with a 10K pull-down resistor.
+
+`solar-sensors.py` scans the sensors every 5 seconds and makes a request to http-keystore, where something else can snag the data.
+
+`solar-sensors.service` goes in /etc/systemd/system:
+
+# Setup
+
+As root on a raspberry pi running Raspbian:
+
+```
+adduser solar
+su - solar
+git clone https://github.com/jonasacres/solar-sensors
+logout
+
+cp ~solar/solar-sensors/solar-sensors.service /etc/systemd/system
+systemctl start solar-sensors
+systemctl enable solar-sensors.service
+```
+
+# http-keystore data
+
+Posted to `/solar-sensors`:
+
+key            | value
+-------------- | -----
+photo_mv       | photoresistor voltage
+solar_mv       | solar panel voltage
+solar_power_mw | solar panel power, milliwatts
+
---
+++ solar-sensors.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+ 
+# Adapted from:
+# https://learn.adafruit.com/reading-a-analog-in-and-controlling-audio-volume-with-the-raspberry-pi
+
+# sudo apt-get install python-setuptools
+# sudo easy_install rpi.gpio
+
+import time
+import os
+import RPi.GPIO as GPIO
+ 
+GPIO.setmode(GPIO.BCM)
+ 
+# read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
+def readadc(adcnum, clockpin, mosipin, misopin, cspin):
+    if ((adcnum > 7) or (adcnum < 0)):
+        return -1
+
+    GPIO.output(cspin, True)
+
+    GPIO.output(clockpin, False)  # start clock low
+    GPIO.output(cspin, False)     # bring CS low
+
+    commandout = adcnum
+    commandout |= 0x18  # start bit + single-ended bit
+    commandout <<= 3    # we only need to send 5 bits here
+    for i in range(5):
+        if (commandout & 0x80):
+            GPIO.output(mosipin, True)
+        else:
+            GPIO.output(mosipin, False)
+        commandout <<= 1
+        GPIO.output(clockpin, True)
+        GPIO.output(clockpin, False)
+
+    adcout = 0
+    # read in one empty bit, one null bit and 10 ADC bits
+    for i in range(12):
+        GPIO.output(clockpin, True)
+        GPIO.output(clockpin, False)
+        adcout <<= 1
+        if (GPIO.input(misopin)):
+            adcout |= 0x1
+
+    GPIO.output(cspin, True)
+    
+    adcout >>= 1       # first bit is 'null' so drop it
+    return adcout
+ 
+# change these as desired - they're the pins connected from the
+# SPI port on the ADC to the Cobbler
+SPICLK = 18
+SPIMISO = 23
+SPIMOSI = 24
+SPICS = 25
+
+PHOTORESISTOR_CHANNEL = 0
+SOLAR_PANEL_CHANNEL = 1
+ 
+# set up the SPI interface pins
+GPIO.setup(SPIMOSI, GPIO.OUT)
+GPIO.setup(SPIMISO, GPIO.IN)
+GPIO.setup(SPICLK, GPIO.OUT)
+GPIO.setup(SPICS, GPIO.OUT)
+ 
+while True:
+    scale = 3.3 / 1023
+    photo_v = round(scale * readadc(PHOTORESISTOR_CHANNEL, SPICLK, SPIMOSI, SPIMISO, SPICS), 3)
+    solar_v = round(scale * readadc(SOLAR_PANEL_CHANNEL, SPICLK, SPIMOSI, SPIMISO, SPICS), 3)
+
+    solar_resistor_ohms = 1000 # TODO: measure
+    solar_power_mw = round(solar_v**2/solar_resistor_ohms, 3)
+
+    http_keystore_url = "http://10.0.1.125:11000/solar-sensors"
+    post_data = {"photo_v":photo_v, "solar_v":solar_v, "solar_power_mw":solar_power_mw}
+    requests.post(http_keystore_url, data=post_data)
+
+    print "photoresistor: ", photo_v, 3, "V"
+    print "  solar power: ", solar_power_mw, 3, "mW"
+    print "solar voltage: ", solar_v, 3, "V"
+
+    time.sleep(5)
